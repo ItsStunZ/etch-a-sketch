df22fd47c6a584611de1f3317543681053901d0a
ItsStunZ <115426921+ItsStunZ@users.noreply.github.com>
2026-03-06 04:52:25 +0000

    Finished working on grid interaction

--- script.js
+++ script.js
@@ -2,11 +2,18 @@ const GRID_CONTAINER = document.querySelector('.grid-container');
 const INPUT_FIELD = document.querySelector('#input-gridSize');
 const INPUT_BTN = document.querySelector('#input-btn');
 
-const gridSize = 16;
-
 let mouseDown = false;
 
-function generateGrid() {
+function clearGrid() {
+    const cells = document.querySelectorAll('.cell');
+    const rows = document.querySelectorAll('.row');
+    cells.forEach((cell) => cell.remove());
+    rows.forEach((row) => row.remove());
+}
+
+function generateGrid(gridSize) {
+    clearGrid();
+
     for (let i = 0; i < gridSize; i++) {
         const row = document.createElement('div');
         row.classList.add('row');
@@ -27,11 +34,28 @@ function generateGrid() {
     }
 }
 
-INPUT_BTN.addEventListener('click', function() {
-    
+INPUT_BTN.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    // remove any alerts. Keeps from building
+    const alerts = document.querySelectorAll('.alert');
+    alerts.forEach((alert) => alert.remove());
+
+    const userInput = parseInt(INPUT_FIELD.value);
+
+    if (userInput > 100 || userInput < 1) {
+        let alert = document.createElement('p');
+        alert.classList.add('alert');
+        alert.textContent = "Can only set the size between 1 and 100!";
+        alert.style.color = '#fc6c6c';
+
+        document.body.appendChild(alert);
+    } else {
+        generateGrid(userInput);
+    }
 })
 
 document.addEventListener('mousedown', () => mouseDown = true);
 document.addEventListener('mouseup', () => mouseDown = false);
 
-generateGrid();
+generateGrid(16);
